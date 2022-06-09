c374ccfd946f0553ae738954dfa213bb0ed0d9ee
Michael Pacleb <michael.pacleb@gmail.com>
2022-06-08 23:27:02 -0400

    Completed solution for exer# 4.3-4.4.

--- part4/blog_list/utils/config.js
+++ part4/blog_list/utils/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 
-module.exports = { 
-  PORT, 
-  MONGODB_URI 
+module.exports = {
+  PORT,
+  MONGODB_URI
 }
