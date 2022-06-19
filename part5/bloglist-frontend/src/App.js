710e606953fb1ba8f11f8fe6cca3e25c8bf050a3
Michael Pacleb <michael.pacleb@gmail.com>
2022-06-19 10:36:40 -0400

    Completed solution for exer# 5.2.

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -9,11 +9,25 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser')
+
+    if (loggedUserJSON){
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+
+      // TODO add logic to pass user.token when posting new blogs in blogService.blog
+    }
+  }, [])
+
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -23,6 +37,7 @@ const App = () => {
         username, password
       })
 
+      window.localStorage.setItem('loggedBloglistUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -30,6 +45,17 @@ const App = () => {
       console.log(exception);
     }
   }
+
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    window.localStorage.clear()
+    setUser(null)
+    setUsername('')
+    setPassword('')
+  }
+  
 
   const loginForm = () => (
     <form onSubmit={handleLogin}>
@@ -72,6 +98,7 @@ const App = () => {
       <div>
         <h2>blogs</h2>
         <p>{user.name} logged in</p>
+        <button onClick={handleLogout} type='button'>logout</button>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
