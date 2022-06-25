61976110694d4fa509a2c58aaf654cec1e74a663
Michael Pacleb <michael.pacleb@gmail.com>
2022-06-25 10:06:20 -0400

    Completed solution for exer# 5.18.

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -16,5 +16,29 @@ describe('Blog app', () => {
     cy.visit('http://localhost:3000')
   })
 
+  describe('Login', function() {
+    it('succeeds with right credentials', function() {
+      cy.get('#username').type('micpac')
+      cy.get('#password').type('fullstack')
+      cy.get('#login-button').click()
+
+      cy.contains('Michael Pacleb logged in')
+    })
+
+    it('fail with wrong credentials', function() {
+      cy.get('#username').type('micpac')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html')
+        .should('not.contain', 'Michael Pacleb logged in')
+    })
+  })
+
 
 })
--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -111,6 +111,7 @@ const App = () => {
       <div>
         username
         <input
+          id='username'
           type='text'
           value={username}
           name='Username'
@@ -121,6 +122,7 @@ const App = () => {
       <div>
         password
         <input
+          id='password'
           type='password'
           value={password}
           name='Password'
@@ -128,7 +130,7 @@ const App = () => {
         />
       </div>
 
-      <button type="submit">login</button>
+      <button id='login-button' type="submit">login</button>
     </form>
   )
 
