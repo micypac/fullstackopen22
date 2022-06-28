364f5331a36950db69e3cb846af0b51bde792200
Michael Pacleb <michael.pacleb@gmail.com>
2022-06-28 00:10:23 -0400

    Completed solution for exer# 6.8.

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { addVotesOf } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div key={anecdote.id}>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  return (
+    <div>
+      {anecdotes.map(anecdote =>
+        <Anecdote key={anecdote.id} anecdote={anecdote} handleClick={() => dispatch(addVotesOf(anecdote.id))} />
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
