aa79931ccae1d73609aeb34cff523cb03ee9568d
Michael Pacleb <michael.pacleb@gmail.com>
2022-07-04 19:12:18 -0400

    Completed solution for exer# 7.2.

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,12 +1,8 @@
 import { useState } from 'react'
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
   Link,
-  Navigate,
-  useParams,
-  useNavigate,
   useMatch
 } from 'react-router-dom'
 
@@ -28,8 +24,20 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
+  </div>
+)
+
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content} by {anecdote.author}</h2>
+    <p>has {anecdote.votes} votes</p>
+    <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
   </div>
 )
 
@@ -114,6 +122,11 @@ const App = () => {
 
   const [notification, setNotification] = useState('')
 
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match
+    ? anecdotes.find(anecdote => anecdote.id === Number(match.params.id))
+    : null
+
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
@@ -136,14 +149,13 @@ const App = () => {
   return (
     <div>
       <h1>Software anecdotes</h1>
-      <Router>
         <Menu />
         <Routes>          
           <Route path='/' element={<AnecdoteList anecdotes={anecdotes}/>} />
           <Route path='/about' element={<About />} />
           <Route path='/create' element={<CreateNew addNew={addNew} />} />
+          <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdote} />} />
         </Routes>        
-      </Router>
       
       <Footer />
     </div>
