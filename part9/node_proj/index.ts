3febc8279758342d6a50a112e028220cced7aa98
Michael Pacleb <michael.pacleb@gmail.com>
2023-02-13 09:28:39 -0500

    Solution for exer#9.4 - basic express application using Typescript.

---
+++ part9/node_proj/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
