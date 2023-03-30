983ade906d940219f8164f4bb134dce7e3f560ee
Michael Pacleb <michael.pacleb@gmail.com>
2023-03-29 22:01:35 -0400

    Exer#13.19 - implemented a new table reading_list that connects users and blog via migration.

--- part13/bloglist/models/index.js
+++ part13/bloglist/models/index.js
@@ -1,10 +1,14 @@
 const Blog = require("./blog");
 const User = require("./user");
+const ReadingList = require("./reading_list");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 // Blog.sync({ alter: true });
 // Bloguser.sync({ alter: true });
+
+User.belongsToMany(Blog, { through: ReadingList, as: "read_list" });
+Blog.belongsToMany(User, { through: ReadingList, as: "interested_readers" });
 
 module.exports = {
   Blog,
